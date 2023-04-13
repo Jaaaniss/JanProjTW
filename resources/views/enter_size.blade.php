7c089558eaaee9b4d37c853ee8dc69e61fefe864
Jaaaniss <janis.liedeskalnins@valmierastehnikums.lv>
2023-04-13 12:22:15 +0400

    saaku iet uz funkcionalitati / edit size blade

--- resources/views/enter_size.blade.php
+++ resources/views/enter_size.blade.php
@@ -2,103 +2,82 @@
 @section('content')
 
 
-<div
-            class="py-[250px] dark:bg-[#454547] bg-white dark:shadow-xl shadow-lg my-5 w-full max-w-screen-md lg:max-w-screen-lg relative overflow-x-auto rounded-3xl">
-
-            <div class=" border-b-[1px] dark:border-zinc-500 border-zinc-200 px-8 py-4">
-                <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
-                    Delete Account
-                </h2>
-            </div>
-
-            <div class=" px-8 pb-8 pt-1 ">
+<div class="pt-[300px] pb-[350px] flex flex-col items-center ">
 
 
-                <div class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+
+    <!-- Breadcrumb -->
+    <nav class="mb-2 w-full max-w-screen-md lg:max-w-screen-lg relative overflow-x-auto flex px-5 py-3 dark:bg-[#454547] bg-white dark:shadow-xl shadow-lg rounded-3xl"
+        aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+            <li class="inline-flex items-center">
+                <a href="/"
+                    class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                        </path>
+                    </svg>
+                    Home
+                </a>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                    <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Enter your size</span>
+                </div>
+            </li>
+        </ol>
+    </nav>
+
+
+    <!-- User account deletion -->
+    <div
+        class="dark:bg-[#454547] bg-white dark:shadow-xl shadow-lg my-5 w-full max-w-screen-md lg:max-w-screen-lg relative overflow-x-auto rounded-3xl">
+
+        <div class=" border-b-[1px] dark:border-zinc-500 border-zinc-200 px-8 py-4">
+            <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+                Enter your size
+            </h2>
+        </div>
+
+        <div class=" px-8 pb-8 pt-1 ">
+
+
+            <div class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+
+                <div>
+                    <label for="name" class="block mb-5 text-md font-medium text-gray-900 dark:text-white">
+                        Te var ierakstit izmeru tikai lai to saglabatu, bet var arii meklet internetaa kurpes
+                                    pec taa izmera
+                    </label>
+
 
                     <div>
-                        <label for="name" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">
-                            Once your account is deleted, all of its resources and data will be permanently deleted. Before
-                            deleting your account, please download any data or information that you wish to retain.
-                        </label>
-
-
-                        <form action="{{ url('self-delete') }}" method="POST">
+                        <form action="/enter_size" method="post">
                             @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Permanently
-                                Delete</button>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Foot
+                                size
+                                (cm)
+                            </label>
+                            <div class="relative">
+                                <input id="sizeinput" max="1000"  name="foot_size_cm" type="number" class="block w-full p-3 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#323232] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter a number">
+                                <button type="submit" id="submitbutton" class="h-[30px] flex items-center bg-black dark:text-black text-white absolute right-2 bottom-2 dark:bg-white dark:hover:bg-zinc-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 hover:bg-zinc-700">Enter</button>
+                            </div>
                         </form>
-
-
                     </div>
-                </div>
 
+
+                </div>
             </div>
 
         </div>
 
-
-
-
-
-
-
-
-
-
-        <div style="display: flex;align-items: center;justify-content: center;height: 100%;" class="py-[250px] container">
-
-            <div class="lielaiscontainer">
-
-                <div class="row">
-                    <div class="col">
-                        <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
-                            <ol class="breadcrumb mb-0">
-                                <li class="breadcrumb-item"><a id="a" href="/">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Enter size</li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-
-                <div class="row">
-
-                    <div class="col-lg-8">
-                        <div id="cardpicture" class="card mb-4">
-                            <div style="display: flex;flex-direction: column;justify-content: center;" class="card-body text-center">
-                                <h3>
-                                    <center>
-                                        Te var ierakstit izmeru tikai lai to saglabatu, bet var arii meklet internetaa kurpes pec taa izmera
-                                    </center>
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <div id="cardinfo" class="card mb-4">
-                            <div style="padding-top: 38px; padding-bottom: 38px;;"class="card-body">
-                                <div style="justify-content:center" class="row">
-                                    <form class="formss" action="/enter_size" method="post">
-                                        @csrf
-                                        <input id="sizeinput" max="1000" type="number" name="foot_size_cm" class="form-control"
-                                            placeholder="Enter a number" aria-label="Cm" aria-describedby="button-addon2">
-                                        <div style="display: flex;flex-direction: column;padding-bottom: 20p;" class="butooni">
-                                            <div style="padding-bottom: 10px; padding-top: 10px;" class="viensbuttons">
-                                                <button style="width: 100%!important;" class="btn btnneed btn-dark" type="submit" id="submitbutton">Save</button>
-                                            </div>
-                                            <a id="nulll" style="width: 100%!important;" href="/start_pages/size_converter" class="btn btnneed btn-dark" role="button"><span>Next</span></a>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
+    </div>
 @endsection
--- resources/views/search_size.blade.php
+++ resources/views/search_size.blade.php
@@ -1,61 +1,111 @@
 @extends('layouts.app')
 @section('content')
+    <div class="pt-[200px] pb-[220px] flex flex-col items-center ">
 
 
 
-        <div style="display: flex;align-items: center;justify-content: center;height: 100%;" class="py-[250px] container">
-
-            <div class="lielaiscontainer">
-
-                <div class="row">
-                    <div class="col">
-                        <nav aria-label="breadcrumb" class="bg-light rounded-3 p-3 mb-4">
-                            <ol class="breadcrumb mb-0">
-                                <li class="breadcrumb-item"><a id="a" href="/">Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Search your size</li>
-                            </ol>
-                        </nav>
+        <!-- Breadcrumb -->
+        <nav class="mb-2 w-full max-w-screen-md lg:max-w-screen-lg relative overflow-x-auto flex px-5 py-3 dark:bg-[#454547] bg-white dark:shadow-xl shadow-lg rounded-3xl"
+            aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li class="inline-flex items-center">
+                    <a href="/"
+                        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <svg aria-hidden="true" class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                            </path>
+                        </svg>
+                        Home
+                    </a>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Search your
+                            size</span>
                     </div>
-                </div>
+                </li>
+            </ol>
+        </nav>
 
-                <div class="row">
 
-                    <div class="col-lg-8">
-                        <div id="cardpicture" class="card mb-4">
-                            <div style="display: flex;flex-direction: column;justify-content: center;" class="card-body text-center">
-                                <h3 style="text-align: center;">
-                                    Saved foot size is {{ auth()->user()->foot_size_cm }}cm
-                                </h3>
-                                <h3 style="padding-top: 40px;">
-                                    <center>Seit varetu ievadit savu izmeru, dzimti, zimolu un meklet veikalus kados ir tas izmers</center>
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
+        <!-- User account deletion -->
+        <div
+            class="dark:bg-[#454547] bg-white dark:shadow-xl shadow-lg my-5 w-full max-w-screen-md lg:max-w-screen-lg relative overflow-x-auto rounded-3xl">
 
-                    <div class="col-lg-4">
-                        <div id="cardinfo" class="card mb-4">
-                            <div style="padding-top: 38px; padding-bottom: 38px;;"class="card-body">
-                                <div style="justify-content:center" class="row">
-                                    <form class="formss" action="/enter_size" method="post">
-                                        @csrf
-                                        <input id="sizeinput" max="1000" type="number" name="foot_size_cm" class="form-control"
-                                            placeholder="Enter a number" aria-label="Cm" aria-describedby="button-addon2">
-                                        <div style="display: flex;flex-direction: column;padding-bottom: 20p;" class="butooni">
-                                            <div style="padding-bottom: 10px; padding-top: 10px;" class="viensbuttons">
-                                                <button style="width: 100%!important;" class="btn btnneed btn-dark" type="submit" id="submitbutton">Save</button>
-                                            </div>
-                                            <a id="nulll" style="width: 100%!important;" href="/start_pages/size_converter" class="btn btnneed btn-dark" role="button"><span>Next</span></a>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+            <div class=" border-b-[1px] dark:border-zinc-500 border-zinc-200 px-8 py-4">
+                <h2 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+                    Search your size
+                </h2>
             </div>
+
+            <div class=" px-8 pb-8 pt-1 ">
+
+
+                <div class="mt-4 space-y-4 lg:mt-5 md:space-y-5">
+
+                    <div>
+
+                        <p class="mb-5 text-lg dark:text-white">
+                            Your saved foot size is {{ auth()->user()->foot_size_cm }}cm
+                        </p>
+
+                        <label for="name" class="block mb-5 text-md font-medium text-gray-900 dark:text-white">
+                            Seit varetu ievadit savu izmeru, dzimti, zimolu un meklet veikalus kados ir tas izmers
+                        </label>
+
+
+                        <div>
+                            <form action="/enter_size" method="post">
+                                @csrf
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Foot
+                                    size
+                                    (cm)
+                                </label>
+                                <div class="relative">
+                                    <input id="sizeinput" max="1000" name="foot_size_cm" type="number"
+                                        class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#323232] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Enter a number">
+                                    <button type="submit"
+                                        class="text-white dark:text-black absolute right-2.5 bottom-2.5 bg-black hover:bg-zinc-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-white dark:hover:bg-zinc-200">Enter</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="mt-3">
+                            <form>
+                                <label for="search"
+                                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                <div class="relative">
+                                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                            fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                                        </svg>
+                                    </div>
+                                    <input type="search" id="search"
+                                        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-xl bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-[#323232] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="Search" required>
+                                    <button type="submit"
+                                        class="text-white dark:text-black absolute right-2.5 bottom-2.5 bg-black hover:bg-zinc-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-white dark:hover:bg-zinc-200">Search</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
         </div>
-
-
-@endsection
+    @endsection
